f43f9a06d4ffe57f2c4e7d217044ca14323e9267
handong <15822895227@163.com>
2019-02-13 14:07:00 +0800

    删除了node-modules里的一些文件，转而用记事本的形式对依赖包进行记录

--- background/js/ex_2.js
+++
@@ -1,3 +0,0 @@
-// document.write("<script language=javascript src='ex_1.js'></script>");
-var b=new_element.setAttribute("src","ex_1.js");
-console.log(b);
--- background/js/login_loop.js
+++
@@ -1,7 +0,0 @@
-loopy()
-	function loopy() {
-	var upwd =""
-	while (upwd != "admin") {
-	upwd = prompt("请输入用户名");
-	}
-}
